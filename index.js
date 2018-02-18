aacec2cd40679c45a7b4ef73ff716706a0571f27
Mohanad Elhag <moh.7afiz@gmail.com>
2018-02-18 18:02:17 +0300

    Max Character

--- index.js
+++ index.js
@@ -99,7 +99,29 @@ function capitalizeLetters(str) {
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {}
+
+// function maxCharacter(str) {
+//   const charMap = {};
+//   let maxNum = 0;
+//   let maxChar = ''
+
+//   str.split('').forEach(function(char){
+//     if(char[char]){
+//       charMap[char]++
+//     } else {
+//       charMap[char] = 1
+//     }
+//   })
+
+//   for(let char in charMap){
+//     // debugger
+//     if(charMap[char] > maxNum){
+//       maxNum = charMap[char];
+//       maxChar = char
+//     }
+//   }
+//   return maxChar;
+// }
 
 /////////////////////////////////////////////////////////////////////////
 
@@ -110,6 +132,6 @@ function fizzBuzz() {}
 
 
 // Call Function
-const output = capitalizeLetters('i love our prophet');
+const output = maxCharacter('i love javascript');
 
 console.log(output)
